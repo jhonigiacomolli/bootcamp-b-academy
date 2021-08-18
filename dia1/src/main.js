3d4a9c1919d527ef10e672db89ad80e41cfc7f9d
jhonigiacomolli <edicao@microsite.net.br>
2021-08-18 08:38:32 -0300

    Dia 2 | Resolução Exercicio 1

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,12 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('[data-js="app"]')
+const link = document.querySelector('[data-js="link"]')
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+link.addEventListener('click', () => {
+  app.style.display === 'none' ? app.style.display = 'block' : app.style.display = 'none'
+  app.style.display === 'none' ? link.innerHTML = 'Exibir App' : link.innerHTML = 'Ocultar App'
+})
