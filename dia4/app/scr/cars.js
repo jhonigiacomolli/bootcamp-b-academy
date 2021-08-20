77d64ebc3bf6c78ae3d60b197fd16c11a79805e8
Jhoni Giacomolli <edicao@microsite.net.br>
2021-08-20 11:48:09 -0300

    Dia 4 | Resolucao Exercicio 3

--- dia4/app/scr/cars.js
+++ dia4/app/scr/cars.js
@@ -1,54 +1,97 @@
 const form = document.querySelector('[data-js="form-cars"]')
 const table = document.querySelector('[data-js="table"]')
+
 const url = 'http://localhost:3333/cars'
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault()
-//   const elements = e.target.elements
-//   const row = document.createElement('tr')
-//   const url = document.createElement('td')
-//   const model = document.createElement('td')
-//   const year = document.createElement('td')
-//   const plate = document.createElement('td')
-//   const color = document.createElement('td')
-//   const img = document.createElement('img')
-//   const colorDiv = document.createElement('div')
-//   colorDiv.style.width = '70px'
-//   colorDiv.style.height = '50px'
-//   colorDiv.style.backgroundColor = elements.color.value
-//   img.setAttribute('src', elements.url.value)
-//   model.innerText = elements.model.value
-//   year.innerText = elements.year.value
-//   plate.innerText = elements.plate.value
-//   color.appendChild(colorDiv)
-//   if(
-//     elements.url.value &&
-//     elements.model.value &&
-//     elements.year.value &&
-//     elements.plate.value &&
-//     elements.color.value
-//   ) {
-//     row.appendChild(img)
-//     row.appendChild(model)
-//     row.appendChild(year)
-//     row.appendChild(plate)
-//     row.appendChild(color)
-//     table.appendChild(row)
 
-//     const inputs = [...elements].map(input => input.value = '')
-//     elements.url.focus()
-//   }else {
-//     alert('Preencha todos os campos para efetuar o cadastro!')
-//   }
-// })
-
-const GetData = async () => {
-  const tr = document.createElement('tr')
+async function GetData () {
   const data = await fetch(url).then(resp => resp.json())
-
-  data.length > 0 ?
-  data.map():
-    tr.innerText = 'Nenhum carro encontrado'
-    tr.setAttribute('colspan', '100%')
+  table.innerHTML = ''
+  if(data.length > 0) {
+    data.map(car => {
+      const tr = document.createElement('tr')
+      const tdImage = document.createElement('td')
+      const img = document.createElement('img')
+      img.setAttribute('src', car.image)
+      tdImage.appendChild(img)
+      const tdModel = document.createElement('td')
+      tdModel.innerText = car.brandModel
+      const tdYear = document.createElement('td')
+      tdYear.innerText = car.year
+      const tdPlate = document.createElement('td')
+      tdPlate.innerText = car.plate
+      const tdColor = document.createElement('td')
+      const colorDiv = document.createElement('div')
+      colorDiv.style.width = '70px'
+      colorDiv.style.height = '50px'
+      colorDiv.style.backgroundColor = car.color
+      tdColor.appendChild(colorDiv)
+      tr.classList.remove('empty')
+      tr.appendChild(tdImage)
+      tr.appendChild(tdModel)
+      tr.appendChild(tdYear)
+      tr.appendChild(tdPlate)
+      tr.appendChild(tdColor)
+      table.appendChild(tr)
+    })
+  }else {
+    const tr = document.createElement('tr')
+    const td = document.createElement('td')
+    td.innerText = 'Nenhum carro encontrado'
+    td.setAttribute('colspan', 5)
+    td.setAttribute('data-js', 'empty-row')
+    tr.appendChild(td)
     table.appendChild(tr)
+  }
 }
 GetData()
+
+async function PostData() {
+  const image = form.elements.url.value
+  const model = form.elements.model.value
+  const year = Number(form.elements.year.value)
+  const plate = form.elements.plate.value
+  const color = form.elements.color.value
+
+  const result = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image: image,
+      brandModel: model,
+      year: year,
+      plate: plate,
+      color: color
+    })
+  }).then(resp => resp.json())
+
+  result.error ? DisplayMessage(result.message) :  DisplayMessage(result.message), GetData()
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  PostData()
+})
+
+
+function DisplayMessage(message) {
+  const body = document.querySelector('body')
+
+  const modal = document.createElement('div')
+  const messageBox = document.createElement('div')
+  const msg = document.createElement('p')
+  const button = document.createElement('button')
+  modal.classList.add('modal')
+  modal.classList.add('open')
+  messageBox.classList.add('message')
+  msg.innerHTML = message
+  messageBox.appendChild(msg)
+  button.textContent = 'OK'
+  button.addEventListener('click', () => {
+    body.removeChild(modal)
+  })
+  messageBox.appendChild(button)
+  modal.appendChild(messageBox)
+  body.appendChild(modal)
+}
